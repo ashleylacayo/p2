8f8670315e1df37ff57d8ab24a5942d334a8ec55
ashleylacayo <58410351+ashleylacayo@users.noreply.github.com>
2020-07-09 18:23:48 -0400

    JS added

---
+++ main.js
@@ -0,0 +1,13 @@
+// $.backstretch("images/beach.jpg") //
+
+$.backstretch([
+    "images/blackgold2.jpg",
+    "images/blackgold3.jpg",
+    "images/blackgold2.jpg",
+    "images/blackgold3.jpg"
+  ],
+
+
+    {duration: 4000, transition: "fade", transitionDuration: 1000}
+
+  );
